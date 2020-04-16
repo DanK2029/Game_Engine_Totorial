3f1526b7c91a1e7a5083383b1b9c43e8a36adafb
Daniel Kane <dpk2029@gmail.com>
2020-04-16 17:23:34 -0400

    Added Premake file and bat file to build project

---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "Game_Engine"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Game_Engine"
+	location "Game_Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"PLATFORM_WINDOWS",
+			"BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DIST"
+		optimize "On"
+
+project "Sandbox"
+location "Game_Engine"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Game_Engine/vendor/spdlog/include",
+		"Game_Engine/src"
+	}
+
+	links {
+		"Game_Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DIST"
+		optimize "On"
